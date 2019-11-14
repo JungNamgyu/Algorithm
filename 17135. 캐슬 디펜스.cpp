24bfd67f4ff8645271eb5023012e4d033fd7df05
Namgyu <jng6017@naver.com>
2019-11-15 04:54:20 +0900

    Success

---
+++ 17135. 캐슬 디펜스.cpp
@@ -0,0 +1,59 @@
+﻿// 17135. 캐슬 디펜스
+
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+int N, M, D, map[15][15];
+int defense(int, int, int);
+struct Z { int r, c, dis = 11; };
+inline int dis(int r1, int c1, int r2, int c2) { return abs(r1 - r2) + abs(c1 - c2); }
+
+int main() {
+	scanf("%d%d%d", &N, &M, &D);
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			scanf("%d", &map[i][j]);
+
+	int Max = 0;
+	for (int i = 0; i < M; i++)
+		for (int j = i + 1; j < M; j++)
+			for (int k = j + 1; k < M; k++)
+				Max = max(Max, defense(i, j, k));
+
+	printf("%d", Max);
+	return 0;
+}
+
+int defense(int a, int b, int c) {
+	int kill = 0;
+	bool M_ch[15][15] = { false };
+	for (int t = N; t > 0; t--) {
+		Z a_k, b_k, c_k;
+		for (int j = 0; j < M; j++) {
+			for (int i = t - 1; i >= max(0, t - D); i--) {
+				if (map[i][j] && !M_ch[i][j]) {
+					if (a_k.dis > dis(t, a, i, j) && D >= dis(t, a, i, j)) {
+						a_k.r = i, a_k.c = j;
+						a_k.dis = dis(t, a, i, j);
+					}
+					if (b_k.dis > dis(t, b, i, j) && D >= dis(t, b, i, j)) {
+						b_k.r = i, b_k.c = j;
+						b_k.dis = dis(t, b, i, j);
+					}
+					if (c_k.dis > dis(t, c, i, j) && D >= dis(t, c, i, j)) {
+						c_k.r = i, c_k.c = j;
+						c_k.dis = dis(t, c, i, j);
+					}
+				}
+			}
+		}
+		if (a_k.dis < 11) M_ch[a_k.r][a_k.c] = true;
+		if (b_k.dis < 11) M_ch[b_k.r][b_k.c] = true;
+		if (c_k.dis < 11) M_ch[c_k.r][c_k.c] = true;
+	}
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			if (M_ch[i][j]) kill++;
+	return kill;
+}
---
+++ 17281. 야구.cpp
@@ -0,0 +1,43 @@
+﻿// 17281. 야구
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N, P[] = { 1,2,3,4,5,6,7,8,9 }, s[50][10], Max;
+
+int main() {
+
+	cin >> N;
+	for (int j = 0; j < N; j++)
+		for (int i = 1; i <= 9; i++) cin >> s[j][i];
+	do {
+		int S = 0, t = 0, p[9];
+
+		for (int i = 0; i < 3; i++) p[i] = P[i + 1];
+		p[3] = P[0];
+		for (int i = 4; i < 9; i++) p[i] = P[i];
+
+		for (int u = 0; u < N; u++) {
+			int out = 0, base[4] = { 0 };
+			while (out < 3) {
+				base[0] = 1;
+				switch (s[u][p[t]]) {
+				case 4: S += base[0];
+				case 3: S += base[1];
+				case 2: S += base[2];
+				case 1: S += base[3]; break;
+				default: out++;
+				}
+				for (int j = 0; j < s[u][p[t]]; j++) {
+					for (int k = 3; k > 0; k--) base[k] = base[k - 1];
+					base[0] = 0;
+				}
+				t = (t + 1) % 9;
+			}
+		}
+		Max = max(Max, S);
+	} while (next_permutation(P + 1, P + 9));
+	cout << Max;
+	return 0;
+}
