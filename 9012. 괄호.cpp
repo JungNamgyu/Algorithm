9d5b7f7e4b1fea6235579490f93a6dc17fa7de92
JungNamgyu <jng6017@naver.com>
2019-12-15 10:44:18 +0900

    Success

---
+++ 9012. 괄호.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int len;
+		bool check = true;
+		string ps; stack<char> s;
+		cin >> ps;
+		len = ps.length();
+		for (int i = 0; i < len; i++) {
+			if (ps[i] == '(') s.push(ps[i]);
+			else {
+				if (s.empty()) { check = false; break; }
+				s.pop();
+			}
+		}
+		cout << ((check && s.empty()) ? "YES\n" : "NO\n");
+	}
+	return 0;
+}
