e9d2bc4dd56d93dc649e3190d1d82b2ac6f40f8b
JungNamgyu <jng6017@naver.com>
2019-12-16 00:00:20 +0900

    Success

---
+++ 2744. 대소문자 바꾸기.cpp
@@ -0,0 +1,17 @@
+﻿#include<string>
+#include<iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	int  len = s.length();
+	for (int i = 0; i < len; i++) {
+		if (islower(s[i]))
+			s[i] = toupper(s[i]);
+		else
+			s[i] = tolower(s[i]);
+	}
+	cout << s;
+	return 0;
+}
