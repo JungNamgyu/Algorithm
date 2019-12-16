2d1cad133d794c37bff0efba8dcac45322b15909
JungNamgyu <jng6017@naver.com>
2019-12-16 15:58:06 +0900

    Success

---
+++ 4153. 직각삼각형.cpp
@@ -0,0 +1,18 @@
+﻿#include<cmath>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+int main() {
+	while (true) {
+		int line[3];
+		cin >> line[0] >> line[1] >> line[2];
+		if (line[0] == 0) break;
+		sort(line, line + 3);
+		if (pow(line[0], 2) + pow(line[1], 2) == pow(line[2], 2))
+			cout << "right" << endl;
+		else
+			cout << "wrong" << endl;
+	}
+	return 0;
+}
