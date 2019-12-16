68129be42f61bfca005dc8b3dd3ae50905d831fa
JungNamgyu <jng6017@naver.com>
2019-12-17 01:01:15 +0900

    Success

---
+++ 1371. 가장 많은 글자.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<cctype>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	int a[26] = { 0 }, Max = 0, t = 1;
+	while (getline(cin, s)) {
+		int len = s.length();
+		for (int i = 0; i < len; i++)
+			if (isalpha(s[i]))
+				a[s[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++)
+		Max = max(Max, a[i]);
+	for (int i = 0; i < 26; i++)
+		if (Max == a[i]) cout << (char)(i + 'a');
+	return 0;
+}
