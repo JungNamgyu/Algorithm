807305d3f4694555b3b4c0fd577fbe05e2292b62
JungNamgyu <jng6017@naver.com>
2019-12-16 22:13:42 +0900

    Success

---
+++ 1264. 모음의 개수.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string s, v = "aeiouAEIOU";
+	while (true) {
+		getline(cin, s);
+		if (!s.compare("#")) break;
+		int len = s.length(), cnt = 0;
+		for (int i = 0; i < len; i++)
+			if (v.find(s[i]) < 10)
+				cnt++;
+		cout << cnt << endl;
+	}
+	return 0;
+}
