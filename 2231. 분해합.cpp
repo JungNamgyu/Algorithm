455cc7a146fb15febb280a84b422304b41193ff7
JungNamgyu <jng6017@naver.com>
2019-12-17 10:03:46 +0900

    Success

---
+++ 2231. 분해합.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n, D[1000001];
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (!D[i]) {
+			for (int j = i, N, s; j <= n; j = s, D[s] = N) {
+				s = N = j;
+				while (j) {
+					s += j % 10;
+					j /= 10;
+				}
+				if (D[s]) {
+					D[s] = min(D[s], N);
+					break;
+				}
+			}
+		}
+	}
+	cout << D[n];
+	return 0;
+}
