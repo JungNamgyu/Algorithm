5e72ad1cab37fdf80c647a81dda6388e5ba34c26
JungNamgyu <jng6017@naver.com>
2019-12-21 11:38:15 +0900

    Success

---
+++ 2563. 색종이.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int n, sum, p[100][100];
+
+int main() {
+	cin >> n;
+	while (n--) {
+		int y, x;
+		cin >> y >> x;
+		for (int i = y; i < y + 10; i++)
+			for (int j = x; j < x + 10; j++) p[i][j] = 1;
+	}
+	for (int i = 0; i < 100; i++)
+		for (int j = 0; j < 100; j++) sum += p[i][j];
+	cout << sum;
+	return 0;
+}
