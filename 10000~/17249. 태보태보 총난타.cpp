e9983061335fac3ce286f312c0234b5b246886be
JungNamgyu <jng6017@naver.com>
2019-12-22 00:22:17 +0900

    Success

---
+++ 10000~/17249. 태보태보 총난타.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	bool c = false;
+	int len = s.length(), le = 0, ri = 0;
+	for (int i = 0; i < len; i++) {
+		if(s[i] == '@')
+			c ? ri += 1 : le += 1;
+		if (s[i] == '(') 
+			c = true;
+	}
+	cout << le << " " << ri;
+	return 0;
+}
