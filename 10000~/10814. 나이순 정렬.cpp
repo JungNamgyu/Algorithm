bf604bfade40f54aca3d79a2e8a5907f99c6c0f2
JungNamgyu <jng6017@naver.com>
2019-12-24 22:58:17 +0900

    Success

---
+++ 10000~/10814. 나이순 정렬.cpp
@@ -0,0 +1,22 @@
+﻿#include<cstdio>
+#include<algorithm>
+using namespace std;
+
+struct Z {
+	int age;
+	char name[101];
+	bool operator < (const Z &x) const {
+		return age < x.age;
+	}
+}member[100000];
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d%s", &member[i].age, &member[i].name);
+	stable_sort(member, member + N);
+	for (int i = 0; i < N; i++)
+		printf("%d %s\n", member[i].age, member[i].name);
+	return 0;
+}
