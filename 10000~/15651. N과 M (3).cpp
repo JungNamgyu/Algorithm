ad84dade5f812bedaa2633b580a99daa0e95405f
JungNamgyu <jng6017@naver.com>
2019-12-25 22:27:58 +0900

    Success

---
+++ 10000~/15651. N과 M (3).cpp
@@ -0,0 +1,25 @@
+﻿#pragma warning (disable:4996);
+
+#include<cstdio>
+
+int M, N, arr[10];
+
+void Sequence(int inx) {
+	if (inx == N) {
+		for (int i = 0; i < inx; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+	}
+	else {
+		for (int i = 1; i <= M; i++) {
+			arr[inx] = i;
+			Sequence(inx + 1);
+		}
+	}
+}
+
+int main() {
+	scanf("%d%d", &M, &N);
+	Sequence(0);
+	return 0;
+}
