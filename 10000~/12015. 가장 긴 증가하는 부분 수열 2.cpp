66237118d9758fb7461f0946fd0f72b40cdd4326
JungNamgyu <jng6017@naver.com>
2019-12-31 19:53:54 +0900

    Success

---
+++ 10000~/12015. 가장 긴 증가하는 부분 수열 2.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> arr(N), D;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+		int le = 0, ri = D.size() - 1;
+		while (le <= ri) {
+			int mid = (le + ri) / 2;
+			(arr[i] > D[mid]) ? le = mid + 1 : ri = mid - 1;
+		}
+		if (le == D.size())
+			D.push_back(arr[i]);
+		else
+			D[le] = arr[i];
+	}
+	cout << D.size();
+	return 0;
+}
