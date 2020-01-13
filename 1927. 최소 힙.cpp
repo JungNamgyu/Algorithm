ad6b1c1974641439dc6f11f811b50656a2109832
JungNamgyu <jng6017@naver.com>
2020-01-13 21:12:11 +0900

    Success

---
+++ 1927. 최소 힙.cpp
@@ -0,0 +1,26 @@
+﻿#pragma warning (disable:4996)
+
+#include<cstdio>
+#include<queue>
+using namespace std;
+
+int main() {
+	int N;
+	priority_queue <int, vector<int>, greater<int> > Max_heap;
+	scanf("%d", &N);
+	while (N--) {
+		int x;
+		scanf("%d", &x);
+		if (x == 0) {
+			if (Max_heap.empty())
+				printf("%d\n", 0);
+			else {
+				printf("%d\n", Max_heap.top());
+				Max_heap.pop();
+			}
+		}
+		else
+			Max_heap.push(x);
+	}
+	return 0;
+}
