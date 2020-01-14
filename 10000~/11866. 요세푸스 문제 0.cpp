c6eb3a91741baef546df0c183a0c9878c218fc64
JungNamgyu <jng6017@naver.com>
2020-01-14 09:46:39 +0900

    Success

---
+++ 10000~/11866. 요세푸스 문제 0.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+	int N, K;
+	cin >> N >> K;
+	queue <int> Q, ANS;
+	for (int i = 1; i <= N; i++)
+		Q.push(i);
+	for (int cn = 1; !Q.empty(); cn++) {
+		if ((cn % K) > 0)
+			Q.push(Q.front());
+		else
+			ANS.push(Q.front());
+		Q.pop();
+	}
+	cout << '<' << ANS.front(); ANS.pop();
+	while (!ANS.empty()) {
+		cout << ", " << ANS.front();
+		ANS.pop();
+	}
+	cout << '>';
+}
