7d737be0fffdd62ad9d67246d966b09451194682
JungNamgyu <jng6017@naver.com>
2020-01-22 00:18:33 +0900

    Success

---
+++ 2805. 나무 자르기.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable:4996)
+
+#include<cstdio>
+#include<vector>
+using namespace std;
+
+int main() {
+	int N, M;
+	scanf("%d%d", &N, &M);
+	vector <int> Tree(N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &Tree[i]);
+	int ANS, le = 0, ri = 1e9;
+	while (le <= ri) {
+		long long sum = 0;
+		int mid = (le + ri) / 2;
+		for (int i = 0; i < N; i++)
+			sum += (Tree[i] > mid ? Tree[i] - mid : 0);
+		if (sum >= M) {
+			ANS = mid;
+			le = mid + 1;
+		}
+		else
+			ri = mid - 1;
+	}
+	printf("%d", ANS);
+	return 0;
+}
