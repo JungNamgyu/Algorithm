6ed76bf8c0db176fc19e012dad3966ddeb0b39ca
JungNamgyu <jng6017@naver.com>
2020-02-13 15:58:39 +0900

    Success

---
+++ 1976. 여행 가자.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector <int> p;
+
+int Union(int a, int b) {
+	if (p[a] == a && p[b] == b)
+		return p[a] = p[b] = min(p[a], p[b]);
+	else
+		return p[a] = p[b] = Union(p[a], p[b]);
+}
+
+int Find(int x) {
+	if (p[x] == x)
+		return x;
+	else
+		return p[x] = Find(p[x]);
+}
+
+int main() {
+	int n, m;
+	
+	cin >> n >> m;
+	p.resize(n + 1);
+	for (int i = 0; i <= n; i++) p[i] = i;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			int path;
+			cin >> path;
+			if (path == 1) Union(i, j);
+		}
+	}
+
+	int st, en;
+	bool check = true;
+	cin >> st;
+	for(int i=1;i<m;i++){
+		cin >> en;
+		Find(st); Find(en);
+		if (p[st] != p[en])
+			check = false;
+		st = en;
+	}
+	cout << (check ? "YES" : "NO") << endl;
+
+	return 0;
+}
