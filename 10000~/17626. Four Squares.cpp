3c5c4cc2b258aa0ac89ec73f5554dcf0664955e5
JungNamgyu <jng6017@naver.com>
2020-03-05 14:49:43 +0900

    Success

---
+++ 10000~/17626. Four Squares.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int D[50001];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int Min = 2e9;
+		for (int j = 1; j * j <= i; j++) {
+			Min = min(Min, D[i - (j * j)]);
+		}
+		D[i] = Min + 1;
+	}
+	cout << D[n];
+	return 0;
+}
