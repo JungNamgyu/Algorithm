fb89ae35fb4d36c9fa0ecb387833e92a091926f7
JungNamgyu <jng6017@naver.com>
2020-03-06 14:57:06 +0900

    Success

---
+++ 10000~/10984. 내 학점을 구해줘.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        int CC = 0;
+        double GG = 0;
+        for (int i = 0; i < N; i++) {
+            int C;
+            double G;
+            cin >> C >> G;
+            CC += C; GG += (C * G);
+        }
+        cout.precision(1);
+        cout.setf(ios::fixed);
+        cout << CC << " " << GG / CC << endl;
+    }
+    return 0;
+}
