c28a3ef9dedcddd5905638fffb5f5e1e715765e6
JungNamgyu <jng6017@naver.com>
2020-03-06 13:14:04 +0900

    Success

---
+++ 10000~/10768. 특별한 날.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int m, d;
+	cin >> m >> d;
+	if (m < 2 || (m == 2 && d < 18))
+		cout << "Before";
+	else if (m > 2 || (m == 2 && d > 18))
+		cout << "After";
+	else
+		cout << "Special";
+	return 0;
+}
