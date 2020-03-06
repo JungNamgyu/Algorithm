af10653d3c6070d9af715d8fd3cc7ffe7b9a98a5
JungNamgyu <jng6017@naver.com>
2020-03-06 20:49:53 +0900

    Success

---
+++ 3034. 앵그리 창영.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int N, W, H, D;
+    cin >> N >> W >> H;
+    D = sqrt(pow(W, 2) + pow(H, 2));
+    while (N--) {
+        int n;
+        cin >> n;
+        if (n <= W || n <= H || n <= D)
+            cout << "DA" << endl;
+        else
+            cout << "NE" << endl;
+    }
+    return 0;
+}
