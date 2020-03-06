f3ab7793b83fba51d042936a0ba134c8b94a028b
JungNamgyu <jng6017@naver.com>
2020-03-06 10:28:33 +0900

    Success

---
+++ 10000~/11966. 2의 제곱인가.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+
+int check(int x) {
+	for (int i = 0; (1 << i) <= x; i++)
+		if ((1 << i) == x) return 1;
+	return 0;
+}
+int main() {
+	int N;
+	cin >> N;
+	cout << check(N);
+	return 0;
+}
