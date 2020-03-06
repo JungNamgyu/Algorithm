1a95168ec05cf7f43255438a3c5ca1bdc36560a3
JungNamgyu <jng6017@naver.com>
2020-03-06 20:35:53 +0900

    Success

---
+++ 2909. 캔디 구매.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int C, K, x, M = 1;
+    cin >> C >> K;
+    for (int i = 0; i < K; i++) M *= 10;
+    x = (C + (5 * M / 10));
+    x = (double)x / M;
+    x = x * M;
+    cout << x;
+    return 0;
+}
