d4adc2fe7f545bd832fb68cde23a43d7baea34b6
JungNamgyu <jng6017@naver.com>
2020-03-07 12:40:41 +0900

    Success

---
+++ 4458. 첫 글자를 대문자로.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<ctype.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	cin.ignore();
+	while (N--) {
+		char S[31];
+		cin.getline(S, 31);
+		if (islower(S[0])) S[0] = toupper(S[0]);
+		cout << S << endl;
+	}
+	return 0;
+}
