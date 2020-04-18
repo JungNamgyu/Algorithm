fbaabe7d078a66b46cbd3bb77d9958845735c923
JungNamgyu <jng6017@naver.com>
2020-04-18 13:00:56 +0900

    Success

---
+++ 2252. 줄 세우기.cpp
@@ -0,0 +1,37 @@
+﻿#pragma warning(disable:4996)
+
+#include<cstdio>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int N, M;
+queue < int> Q;
+vector < int > Cnt;
+vector < vector<int> > Line;
+
+int main() {	
+	scanf("%d%d", &N, &M);
+	Line.resize(N + 1);
+	Cnt.resize(N + 1, 0);
+	for (int i = 0; i < M; i++) {
+		int A, B;
+		scanf("%d%d", &A, &B);
+		Line[A].push_back(B), Cnt[B]++;
+	}
+
+	for (int i = 1; i <= N; i++)
+		if (Cnt[i] == 0) Q.push(i);
+
+	while (!Q.empty()) {
+		int S = Q.front(); Q.pop();
+		printf("%d ", S);
+
+		int sz = Line[S].size();
+		for (int i = 0; i < sz; i++) {
+			Cnt[Line[S][i]]--;
+			if (Cnt[Line[S][i]] == 0) Q.push(Line[S][i]);
+		}
+	}
+	return 0;
+}
