47edba39d5c8b541cf4affc2a1cfe1bb4f3640b9
JungNamgyu <jng6017@naver.com>
2020-06-16 21:00:55 +0900

    Success

---
+++ 1551. 수열의 변화.py
@@ -0,0 +1,10 @@
+N, K = map(int, input().split())
+A = [*map(int, input().split(','))]
+
+for i in range(K):
+    B = []
+    for j in range(len(A)-1):
+        B.append(int(A[j+1]-A[j]))
+    A = B
+
+print(','.join(map(str, A)))
