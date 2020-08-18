4f5e31dc780f072d032d5a6c0039957bc627e428
JungNamgyu <jng6017@naver.com>
2020-08-18 15:22:36 +0900

    Success
    페르마의 소정리, 밀러-라빈 소수판별법

--- Baekjoon Online Judge/5615. 아파트 임대.cpp
+++ Baekjoon Online Judge/5615. 아파트 임대.cpp
@@ -1,4 +1,4 @@
-﻿#include<iostream>
+#include<iostream>
 using namespace std;
 using ull = unsigned long long;
 
@@ -14,36 +14,29 @@ ull modulo(ull a, ull d, ull n) {
 bool miuller_rabin(ull n, ull a) {
 	ull d = n - 1;
 	for (; d % 2 == 0; d /= 2) {
-		if (modulo(a, d, n) == n-1)
+		if (modulo(a, d, n) == n - 1)
 			return true;
 	}
-
 	ull tmp = modulo(a, d, n);
-	return (tmp == 1 || tmp == n-1);
+	return (tmp == 1 || tmp == n - 1);
 }
 
 bool is_prime(ull n) {
-	for (auto a : { 2, 3, 5, 7, 11 }) {
-		if (n == a)
-			return true;
+	for (auto a : { 2, 3, 5, 7, 11 })
 		if (!miuller_rabin(n, a))
 			return false;
-	}
 	return true;
 }
 
 int main() {
 	int N, ans = 0;
-
 	cin >> N;
 	while (N--) {
 		ull s;
-
 		cin >> s;
 		if (is_prime(2 * s + 1))
 			ans = ans + 1;
 	}
 	cout << ans;
-
 	return 0;
 }
