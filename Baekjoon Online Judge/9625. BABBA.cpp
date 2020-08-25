4002346c93e822b0012e90ac26267802fd48654d
JungNamgyu <jng6017@naver.com>
2020-08-25 21:46:27 +0900

    Success

---
+++ Baekjoon Online Judge/9625. BABBA.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int K, D[46][2];
+
+	cin >> K;
+	D[0][0] = 1, D[0][1] = 0;
+	for (int i = 1; i <= K; i++) {
+		D[i][0] = D[i - 1][1];
+		D[i][1] = D[i - 1][0] + D[i - 1][1];
+	}
+	cout << D[K][0] << " " << D[K][1];
+
+	return 0;
+}
