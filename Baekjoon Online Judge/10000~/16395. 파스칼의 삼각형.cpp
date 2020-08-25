ab5f79543cc5563fcc388013c2fd3ae226189200
JungNamgyu <jng6017@naver.com>
2020-08-25 23:49:57 +0900

    Success

---
+++ Baekjoon Online Judge/10000~/16395. 파스칼의 삼각형.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int n, k, pascal[31][31] = { 1, };
+
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= i; j++)
+			pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
+	cout << pascal[n][k];
+
+	return 0;
+}
